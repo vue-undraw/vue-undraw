3443e05526d5a57d9ce88b6e1a622f0d7048c590
Kai Wedekind <wedekind.kai@googlemail.com>
2018-09-08 21:57:32 +0200

    Add component references

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,51 @@
 import UndrawCreditCard from './UndrawCreditCard.vue';
+import UndrawSocialMedia from './UndrawSocialMedia.vue';
+import UndrawMailSent from './UndrawMailSent.vue';
+import UndrawSearch from './UndrawSearch.vue';
+import UndrawMapDark from './UndrawMapDark.vue';
+import UndrawPhoto from './UndrawPhoto.vue';
+import UndrawBlogPost from './UndrawBlogPost.vue';
+import UndrawCharts from './UndrawCharts.vue';
+import UndrawDevices from './UndrawDevices.vue';
+import UndrawAtWork from './UndrawAtWork.vue';
+import UndrawDelivery from './UndrawDelivery.vue';
+import UndrawChat from './UndrawChat.vue';
+import UndrawDocuments from './UndrawDocuments.vue';
+import UndrawAnalytics from './UndrawAnalytics.vue';
+import UndrawPortfolio from './UndrawPortfolio.vue';
+import UndrawPlainCreditCard from './UndrawPlainCreditCard.vue';
+import UndrawYacht from './UndrawYacht.vue';
+import UndrawTabs from './UndrawTabs.vue';
+import UndrawImageFolder from './UndrawImageFolder.vue';
+import UndrawMobile from './UndrawMobile.vue';
+import UndrawPosts from './UndrawPosts.vue';
+import UndrawPhotos from './UndrawPhotos.vue';
+import UndrawMapLight from './UndrawMapLight.vue';
+import UndrawWoman from './UndrawWoman.vue';
 
-const Components = {
-  UndrawCreditCard
+export default {
+  UndrawCreditCard,
+  UndrawSocialMedia,
+  UndrawMailSent,
+  UndrawSearch,
+  UndrawMapDark,
+  UndrawPhoto,
+  UndrawBlogPost,
+  UndrawCharts,
+  UndrawDevices,
+  UndrawAtWork,
+  UndrawDelivery,
+  UndrawChat,
+  UndrawDocuments,
+  UndrawAnalytics,
+  UndrawPortfolio,
+  UndrawPlainCreditCard,
+  UndrawYacht,
+  UndrawTabs,
+  UndrawImageFolder,
+  UndrawMobile,
+  UndrawPosts,
+  UndrawPhotos,
+  UndrawMapLight,
+  UndrawWoman
 };
-
-export default Components;
